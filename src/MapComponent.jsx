eff7f60aa9a5e294a453646efd8084c406cf614a
Your GitHub Name <rasimabilov00@gmail.com>
2026-03-03 13:24:29 +0100

    Final thesis version

---
+++ src/MapComponent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapComponent = () => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>Hello from React-Leaflet!</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
